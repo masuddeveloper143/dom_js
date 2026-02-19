62e9e0ada896fc1290cd4b63701d189ab60c0b68
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-19 22:28:34 +0800

    Update project

--- first.js
+++ first.js
@@ -2,3 +2,7 @@ console.log('this is the first js files');
 let allli = document.getElementsByTagName('li');
 
 console.log(allli);
+
+let sectionTitle = document.getElementsByClassName('section-title');
+console.log(sectionTitle);
+
