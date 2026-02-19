72931cb217c0c5d5b34440009b7252ad49787229
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-19 23:51:40 +0800

    Update project

--- third.js
+++ third.js
@@ -1,2 +1,18 @@
 let main = document.getElementById("maincontainer");
 
+let section = document.createElement('section');
+
+section.innerHTML = `
+
+<h1> my dynamic section <h1/>
+<p>extra text added inside petagreph <p/>
+<ul> 
+<li>first item<li/>
+<li>second item<li/>
+<li>tihrd item<li/>
+<li>four item<li/>
+<li>fuve item<li/>
+<ul/>
+`
+
+main.appendChild(section);
