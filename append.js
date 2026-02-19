b87d1ef61609ed2acce4c50fedf514ac85a69304
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-19 17:14:13 +0800

    Update project

--- append.js
+++ append.js
@@ -8,12 +8,19 @@ li.innerText = 'you know who I am i';
 placeList.appendChild(li);
 
 
-
+// 1. where to add 
 let maincontainer = document.getElementById('maincontainer');
 // console.log(maincontainer);
+
+// 2. what to be added 
 let section = document.createElement('section');
 let h1 = document.createElement('h1');
 h1.innerText = 'section';
 section.appendChild(h1);
+
+let ul = document.createElement('ul');
+let li1 = document.createElement('li');
+li1.innerText('i am from in bangladesh')
+
 
 maincontainer.appendChild(section);
