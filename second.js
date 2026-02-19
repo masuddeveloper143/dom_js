596e3f151623a636240046d9658ae9802d91ad7a
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-19 23:19:12 +0800

    Update project

--- second.js
+++ second.js
@@ -1,3 +1,8 @@
 console.log('this is the second js files');
 
 let secondList = document.getElementById('second-list');
+
+let li = document.createElement('li');
+li.innerText = 'my dynamic item'
+
+secondList.appendChild(li);
