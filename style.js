67f897c3a7c9d7d7a172518950a201d181aeb65b
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-19 14:41:52 +0800

    Update project

--- style.js
+++ style.js
@@ -1,32 +1,32 @@
-// let section = document.querySelectorAll('section');
-// console.log(section);
+// // let section = document.querySelectorAll('section');
+// // console.log(section);
+
+
+// // let sections = document.querySelectorAll('section');
+
+// // for (let section of sections) {
+// //     console.log(section);
+// // }
 
 
 // let sections = document.querySelectorAll('section');
 
 // for (let section of sections) {
-//     console.log(section);
+//     section.style.border = '3px solid red';
+//     section.style.marginBottom = '4rem';
+//     section.style.borderRadius = '10px';
+//     section.style.paddingLeft = '7px';
+//     section.style.backgroundColor = 'lightgray';
 // }
 
 
-let sections = document.querySelectorAll('section');
+// // let placesId = document.getElementById('placesilikevisit');
 
-for (let section of sections) {
-    section.style.border = '3px solid red';
-    section.style.marginBottom = '4rem';
-    section.style.borderRadius = '10px';
-    section.style.paddingLeft = '7px';
-    section.style.backgroundColor = 'lightgray';
-}
+// // placesId.style.backgroundColor = 'yellow';
 
+// let placesContainer = document.getElementById('placesilikevisit');
 
-// let placesId = document.getElementById('placesilikevisit');
-
-// placesId.style.backgroundColor = 'yellow';
-
-let placesContainer = document.getElementById('placesilikevisit');
-
-placesContainer.classList.add('text-center');
+// placesContainer.classList.add('text-center');
 
 // placesContainer.classList.remove('text-color');
 
