742031caeeae872f10474fd66937d61fcfa65ef9
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-19 14:44:24 +0800

    Update project

--- style.js
+++ style.js
@@ -1,33 +1,33 @@
-// // let section = document.querySelectorAll('section');
-// // console.log(section);
-
-
-// // let sections = document.querySelectorAll('section');
-
-// // for (let section of sections) {
-// //     console.log(section);
-// // }
+// let section = document.querySelectorAll('section');
+// console.log(section);
 
 
 // let sections = document.querySelectorAll('section');
 
 // for (let section of sections) {
-//     section.style.border = '3px solid red';
-//     section.style.marginBottom = '4rem';
-//     section.style.borderRadius = '10px';
-//     section.style.paddingLeft = '7px';
-//     section.style.backgroundColor = 'lightgray';
+//     console.log(section);
 // }
 
 
-// // let placesId = document.getElementById('placesilikevisit');
+let sections = document.querySelectorAll('section');
 
-// // placesId.style.backgroundColor = 'yellow';
+for (let section of sections) {
+    section.style.border = '3px solid red';
+    section.style.marginBottom = '4rem';
+    section.style.borderRadius = '10px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'lightgray';
+}
 
-// let placesContainer = document.getElementById('placesilikevisit');
 
-// placesContainer.classList.add('text-center');
+// let placesId = document.getElementById('placesilikevisit');
 
-// placesContainer.classList.remove('text-color');
+// placesId.style.backgroundColor = 'yellow';
+
+let placesContainer = document.getElementById('placesilikevisit');
+
+placesContainer.classList.remove('text-center');
+
+placesContainer.classList.remove('text-color');
 
 
