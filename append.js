312aa79603dbc41e1261e30c77a8c01a8bc21c15
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-19 16:50:46 +0800

    Update project

--- append.js
+++ append.js
@@ -1,6 +1,8 @@
-
+// 1. where to add 
 let placeList = document.getElementById('places-list');
-
+// 2. what to be added 
 let li = document.createElement('li');
 li.innerText = 'you know who I am i';
+
+// 3. add the child 
 placeList.appendChild(li);
